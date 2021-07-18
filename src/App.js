104c4d289d274cac1890d29327656889a47b48b9
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-07-18 14:27:34 -0300

    feat: Add new Task

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ addTask, setAddTask ] = useState("");
   const [ tasks, setTasks ] = useState([]);
   const [ completedTask, setCompletedTask ] = useState([]);
-  
+
+  const handleAddTask = () => {
+    setTasks([...tasks, addTask])
+  }
+
   return (
     <div>
-      
+      <div className="addTaskplace">
+        <input 
+                placeholder="New Task Name"
+                value={addTask}
+                onChange={(e) => setAddTask(e.target.value) }    
+        />
+        <button onClick={() => handleAddTask()}>+ New Task</button>
+      </div>
     </div>
   );
 }
